05efe473f4cca1521f22d1a12724d02938b1a133
lordofgraves <achrafboutzate@gmail.com>
2023-03-21 17:33:59 -0700

    jj

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * main take value
+ * Return always 0
+ */
+int main(void)
+{
+	char *str = "_putchar";
+
+	while (*str)
+	{
+		_putchar(*str)
+			str++;
+	}
+	_putchar('\n');
+
+	return (0);
+}
