bb3fca2040583a0e7e14be9b3d7f9795f6f5b69b
lordofgraves <achrafboutzate@gmail.com>
2023-03-24 16:10:49 -0700

    mm

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -1,25 +1,24 @@
 #include <stdio.h>
+
 /**
  * main -  checks for checks for a digit (0 through 9).
  *
  * Return: Always 0.
  */
-int main()
+int main(void)
 {
-    long long n = 612852475143;
-    long long i, largest_factor;
+	long x, y;
 
-    while (n % 2 == 0) {
-        largest_factor = 2;
-        n = n / 2;
-    }
-    for (i = 3; i <= n; i = i + 2) {
-        while (n % i == 0) {
-            largest_factor = i;
-            n = n / i;
-        }
-    }
+	x = 612852475143;
 
-    printf("%lld\n", largest_factor);
-    return 0;
+	for (y = 2; x > y; y++)
+	{
+		while (x % y == 0)
+		{
+			x = x / y;
+		}
+	}
+	printf("%lu", y);
+	putchar('\n');
+	return (0);
 }
