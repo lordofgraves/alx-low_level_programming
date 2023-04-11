57c77c7d9c0bc5c9d769b54413e7682059d8592c
lordofgraves <achrafboutzate@gmail.com>
2023-04-11 09:37:50 -0700

    mm

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -10,39 +10,43 @@
  */
 char *str_concat(char *s1, char *s2)
 {
-	char *result;
-        unsigned int leng1 = 0, leng2 = 0;
-        if (s1)
-        {
-		while (s1[leng1])
-			leng1++;
-        }
-        else
-        {
-	        s1 = "";
-        }
-        if (s2)
-        {
-	        while (s2[leng2])
-		        leng2++;
-        }
-        else
-        {
-	        s2 = "";
-        }
-        result = (char *) malloc(sizeof(char) * (leng1 + leng2 + 1));
-        if (result == NULL)
-	        return (NULL);
-        if (s1)
-        {
-	        for (leng1 = 0; s1[leng1]; ++leng1)
-		        result[leng1] = s1[leng1];
-        }
-        if (s2)
-        {
-	        for (leng2 = 0; s2[leng2]; ++leng2)
-		        result[leng1 + leng2] = s2[leng2];
-        }
-	result[leng1 + leng2] = '\0';
-	return (result);
+	unsigned int len1 = 0;
+	unsigned int len2 = 0;
+	char *cat;
+
+	if (s1)
+	{
+		while (s1[len1])
+			++len1;
+	}
+	else
+	{
+		s1 = "";
+	}
+	if (s2)
+	{
+		while (s2[len2])
+			++len2;
+	}
+	else
+	{
+		s2 = "";
+	}
+
+	cat = (char *) malloc(sizeof(char) * (len1 + len2 + 1));
+
+	if (!cat)
+		return (NULL);
+	if (s1)
+	{
+		for (len1 = 0; s1[len1]; ++len1)
+			cat[len1] = s1[len1];
+	}
+	if (s2)
+	{
+		for (len2 = 0; s2[len2]; ++len2)
+			cat[len1 + len2] = s2[len2];
+	}
+	cat[len1 + len2] = '\0';
+	return (cat);
 }
