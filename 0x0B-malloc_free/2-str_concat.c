8c7d2748c783f12641acda58b5be4b5125f5df73
lordofgraves <achrafboutzate@gmail.com>
2023-04-11 09:35:46 -0700

    mm

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -10,39 +10,39 @@
  */
 char *str_concat(char *s1, char *s2)
 {
-    char *result;
-    unsigned int leng1 = 0, leng2 = 0;
-    if (s1)
-    {
-	    while (s1[leng1])
-		    leng1++;
-    }
-    else
-    {
-	    s1 = "";
-    }
-    if (s2)
-    {
-	    while (s2[leng2])
-		    leng2++;
-    }
-    else
-    {
-	    s2 = "";
-    }
-    result = (char *) malloc(sizeof(char) * (leng1 + leng2 + 1));
-    if (result == NULL)
-	    return (NULL);
-    if (s1)
-    {
-	    for (leng1 = 0; s1[leng1]; ++leng1)
-		    result[leng1] = s1[leng1];
-    }
-    if (s2)
-    {
-	    for (leng2 = 0; s2[leng2]; ++leng2)
-		    result[leng1 + leng2] = s2[leng2];
-    }
+	char *result;
+        unsigned int leng1 = 0, leng2 = 0;
+        if (s1)
+        {
+		while (s1[leng1])
+			leng1++;
+        }
+        else
+        {
+	        s1 = "";
+        }
+        if (s2)
+        {
+	        while (s2[leng2])
+		        leng2++;
+        }
+        else
+        {
+	        s2 = "";
+        }
+        result = (char *) malloc(sizeof(char) * (leng1 + leng2 + 1));
+        if (result == NULL)
+	        return (NULL);
+        if (s1)
+        {
+	        for (leng1 = 0; s1[leng1]; ++leng1)
+		        result[leng1] = s1[leng1];
+        }
+        if (s2)
+        {
+	        for (leng2 = 0; s2[leng2]; ++leng2)
+		        result[leng1 + leng2] = s2[leng2];
+        }
 	result[leng1 + leng2] = '\0';
 	return (result);
 }
