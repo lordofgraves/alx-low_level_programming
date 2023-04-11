c8e98dbe17f148bd93ecb842e858dc591d074bb6
lordofgraves <achrafboutzate@gmail.com>
2023-04-11 09:19:41 -0700

    mm

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -8,39 +8,36 @@
  * Return: NULL if memory allocation fails,
  * otherwise a pointer to a the new string
  */
-char *str_concat(char *s1, char *s2) 
+char *str_concat(char *s1, char *s2)
 {
-	char *result;
-	int i, j, leng1 = 0, leng2 = 0;
+    char *result;
+    int leng1 = 0, leng2 = 0, i = 0, j = 0;
 
-	if ( s1 == NULL )
-	{
-		s1 = "";
-	}
-	if ( s1 == NULL )
-	{
-		s2 = "";
-	}
-	while (s1[leng1] != '\0')
-	{
-		leng1++;
-	}
-	while (s2[leng2] != '\0')
-	{
-		leng2++;
-	}
-	result = (char *) malloc((leng1 + leng2 + 1) * sizeof(char));
-	if (result == NULL)
-	{
-		return NULL;
-	}
-	while (i = 0; i < leng1; i++)
-	{
-		result[i] = s1[i];
-	}
-	while (j = 0; j < leng2; j++)
-	{
-		result[i+j] = s2[j];
-	}
-	return result;
+    if (s1 == NULL)
+        s1 = "";
+    if (s2 == NULL)
+        s2 = "";
+
+    while (s1[leng1])
+        leng1++;
+    while (s2[leng2])
+        leng2++;
+
+    result = malloc(sizeof(char) * (leng1 + leng2 + 1));
+    if (result == NULL)
+        return (NULL);
+
+    while (i == 0 || i < leng1)
+    {
+        result[i] = s1[i];
+        i++;
+    }
+    while (j == 0 || j < leng2)
+    {
+        result[i + j] = s2[j];
+        j++;
+    }
+    result[i + j] = '\0';
+
+    return (result);
 }
