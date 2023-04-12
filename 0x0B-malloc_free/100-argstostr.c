16a376281404d3e6760231b573a605490f199c32
lordofgraves <achrafboutzate@gmail.com>
2023-04-11 18:12:52 -0700

    mm

---
+++ 0x0B-malloc_free/100-argstostr.c
@@ -0,0 +1,45 @@
+#include "main.h"
+/**
+ * argstostr - concatenate all arguments into a new string
+ * @ac: the argument count
+ * @av: the argument vector
+ *
+ * Return: NULL memory allocation fails, ac is 0 or av is NULL,
+ * otherwise return a pointer to the new string
+ */
+char *argstostr(int ac, char **av)
+{
+	unsigned int i, j, len1 = 0, len2 = 0;
+	char *gather;
+
+	if (ac == 0 || av == NULL)
+	{
+		return (NULL);
+	}
+	for (i = 0; i < ac; i++)
+	{
+		for (j = 0; av[i][j]; j++)
+		{
+			len2++;
+		}
+		len2++;
+	}
+	gather = (char *)malloc(sizeof(char) * (len1 + len2 + 1));
+	if (gather == NULL)
+	{
+		return (NULL);
+	}
+	len2 = 0;
+	for (i = 0; i < ac; i++) 
+	{
+		for (j = 0; av[i][j]; j++) 
+		{
+			gather[len2] = av[i][j];
+			len2++;
+		}
+		gather[len2] = '\n';
+		len2++;
+	}
+	gather[len2] = '\0';
+	return gather;
+}
