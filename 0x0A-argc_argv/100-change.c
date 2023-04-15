fa5e5f1ecda2bde2f2b6f0f10203db0544cc82a6
lordofgraves <achrafboutzate@gmail.com>
2023-04-15 16:17:55 -0700

    mm

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -7,6 +7,7 @@
  * @argv: number of variables to calculate
  * Return: always 0 for sucess
  */
+
 int main(int argc, char **argv)
 {
 	int total, change;
@@ -16,32 +17,22 @@ int main(int argc, char **argv)
 		printf("Error\n");
 		return (1);
 	}
-	change = atoi(argc[1]);
+
+	change = atoi(argv[1]);
+
 	for (total = 0; change > 0; total++)
 	{
-		switch (change)
-		{
-			case 25:
-				change -= 25;
-				break;
-			case 10:
-				change -= 10;
-				break;
-			case 5:
-				change -= 5;
-				break;
-			case 2:
-				change -= 2;
-				break;
-			case 1:
-				change -= 1;
-				break;
-			default:
-				printf("Invalid change amout\n");
-				return (1);
-		}
+		if (change - 25 >= 0)
+			change = change - 25;
+		else if (change - 10 >= 0)
+			change = change - 10;
+		else if (change - 5 >= 0)
+			change = change - 5;
+		else if (change - 2 >= 0)
+			change = change - 2;
+		else if (change - 1 >= 0)
+			change = change - 1;
 	}
 	printf("%d\n", total);
 	return (0);
 }
-
