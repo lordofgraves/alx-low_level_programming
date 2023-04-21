3a95b34f50f2b70767a7a92a85b300f6d76a9592
lordofgraves <achrafboutzate@gmail.com>
2023-04-21 10:43:23 -0700

    mm

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -1,4 +1,5 @@
-#include "variadic_functions.h"
+#include <stdarg.h>
+#include <stdio.h>
 
 /**
  * print_all - prints anything.
@@ -6,46 +7,21 @@
  *
  * Return: no return.
  */
-void print_all(const char * const format, ...)
-{
-	va_list valist;
-	unsigned int i = 0, j, c = 0;
-	char *str;
-	const char t_arg[] = "cifs";
 
-	va_start(valist, format);
-	while (format && format[i])
-	{
-		j = 0;
-		while (t_arg[j])
-		{
-			if (format[i] == t_arg[j] && c)
-			{
-				printf(", ");
-				break;
-			} j++;
-		}
-		switch (format[i])
-		{
-		case 'c':
-			printf("%c", va_arg(valist, int)), c = 1;
-			break;
-		case 'i':
-			printf("%d", va_arg(valist, int)), c = 1;
-			break;
-		case 'f':
-			printf("%f", va_arg(valist, double)), c = 1;
-			break;
-		case 's':
-			str = va_arg(valist, char *), c = 1;
-			if (!str)
-			{
-				printf("(nil)");
-				break;
-			}
-			printf("%s", str);
-			break;
-		} i++;
-	}
-	printf("\n"), va_end(valist);
+void print_all(const char* const format, ...)
+{
+    va_list args;
+    va_start(args, format);
+
+    while (*format)
+    {
+        if (*format == 'c') printf("%c", (char) va_arg(args, int));
+        else if (*format == 'i') printf("%d", va_arg(args, int));
+        else if (*format == 'f') printf("%f", (float) va_arg(args, double));
+        else if (*format == 's') printf("%s", va_arg(args, char*) ? : "(nil)");
+        format++;
+        if (*format && (*format == 'c' || *format == 'i' || *format == 'f' || *format == 's')) printf(", ");
+    }
+    printf("\n");
+    va_end(args);
 }
