6bedfdc8eec915c9c035f647c67a410aa4cf0d6b
lordofgraves <achrafboutzate@gmail.com>
2023-04-27 17:57:35 -0700

    mm

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -1,47 +1,36 @@
 #include "lists.h"
 #include <stdlib.h>
-#include <stddef.h>
 #include <string.h>
 
 /**
- * add_node_end - adds a new node to the end of a list_t list
- * @head: a pointer to a pointer to the head node
- * @str: the string to be stored in the new node
+ * add_node - adds a new node at the beginning of a list_t list.
+ * @head: pointer to the head of the list.
+ * @str: string to be added to the list.
  *
- * Return: a pointer to the newly added node, or NULL on failure
+ * Return: pointer to the new node on success, NULL on failure.
  */
-list_t *add_node_end(list_t **head, const char *str)
+list_t *add_node(list_t **head, const char *str)
 {
-	list_t *new_node, *current_node;
+    if (head == NULL || str == NULL)
+        return (NULL);
 
-	if (head == NULL || str == NULL)
-		return (NULL);
+    size_t len = strlen(str);
 
-	new_node = malloc(sizeof(list_t));
-	if (new_node == NULL)
-		return (NULL);
+    list_t *new_node = malloc(sizeof(list_t));
+    if (new_node == NULL)
+        return (NULL);
 
-	new_node->str = strdup(str);
-	if (new_node->str == NULL)
-	{
-		free(new_node);
-		return (NULL);
-	}
+    char *content = malloc(len + 1);
+    if (content == NULL) {
+        free(new_node);
+        return (NULL);
+    }
+    memcpy(content, str, len + 1);
 
-	new_node->len = strlen(str);
-	new_node->next = NULL;
+    new_node->str = content;
+    new_node->len = len;
+    new_node->next = *head;
+    *head = new_node;
 
-	if (*head == NULL)
-	{
-		*head = new_node;
-	}
-	else
-	{
-		current_node = *head;
-		while (current_node->next != NULL)
-			current_node = current_node->next;
-		current_node->next = new_node;
-	}
-
-	return (new_node);
+    return (new_node);
 }
