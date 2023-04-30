a94a16e31a4d407ed9df5e2ceb19f94043fa0c6a
lordofgraves <achrafboutzate@gmail.com>
2023-04-30 09:16:26 -0700

    mm

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -9,13 +9,14 @@
  */
 size_t print_listint(const listint_t *h);
 {
+	const listint_t *temp = h;
 	size_t count = 0;
 
-	while(h)
+	while(temp != h)
 	{
-		count++;
-		printf("%d\n", h->n);
-		h = h->next;
+		printf("%d\n", temp->n);
+                count++;
+		temp = temp->next;
 	}
 
 	return (count);
