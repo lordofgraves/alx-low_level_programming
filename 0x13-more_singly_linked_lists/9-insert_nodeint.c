d73667c4ee1feec7560d95dbdf26b305dda7aef2
lordofgraves <achrafboutzate@gmail.com>
2023-05-02 19:14:32 -0700

    mm

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -13,9 +13,14 @@
  */
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
-	listint_t *new_node = malloc(sizeof(listint_t));
-	if(new_node == NULL)
-		return(NULL);
+	listint_t *new_node, *current_node = *head;
+
+	if (head == NULL)
+		return NULL;
+
+	new_node = malloc(sizeof(listint_t));
+	if (new_node == NULL)
+		return NULL;
 
 	new_node->n = n;
 
@@ -23,22 +28,23 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	{
 		new_node->next = *head;
 		*head = new_node;
-		return(new_node);
+		return new_node;
 	}
 
-	listint_t *prev_node = *head;
-
-	for(unsigned int i = 0; i < idx - 1; i++)
+	while (idx > 1)
 	{
-		if(prev_node == NULL || prev_node->next == NULL)
+		if (current_node == NULL)
 		{
 			free(new_node);
-			return(NULL);
+			return NULL;
 		}
-		prev_node = prev_node->next;
-	}
-	new_node->next = prev_node->next;
-	prev_node->next = new_node;
 
-	return(new_node);
+		current_node = current_node->next;
+		idx--;
+	}
+
+	new_node->next = current_node->next;
+	current_node->next = new_node;
+
+	return new_node;
 }
