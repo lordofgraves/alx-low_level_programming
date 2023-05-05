105f2cfde7cb762936a7e32f6dba9f5d4b1fe5b1
lordofgraves <achrafboutzate@gmail.com>
2023-05-05 10:49:48 -0700

    mm

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -1,29 +1,21 @@
 #include "main.h"
-#include <unistd.h>
-#include <limits.h>
+
 /**
  * print_binary - prints the binary of a number
- * @n: the number to convert
- * return: 0
+ * @n: long integer
+ *
+ * Return: void.
  */
 void print_binary(unsigned long int n)
 {
-	unsigned long int leftmostbit = 1ul << 63;
-	char p = '0';
-
-	while (!(leftmostbit & n) && leftmostbit != 0)
-		leftmostbit = leftmostbit >> 1;
-
-	if (leftmostbit == 0)
-		write(1, &p, 1);
-
-	while (leftmostbit)
+	if (n >> 0)
 	{
-		if (leftmostbit & n)
-			p = '1';
-		else
-			p = '0';
-		write(1, &p, 1);
-		leftmostbit = leftmostbit >> 1;
+		if (n >> 1)
+			print_binary(n >> 1);
+		_putchar((n & 1) + '0');
+	}
+	else
+	{
+		_putchar('0');
 	}
 }
