dd626afbd5fdc852b29b5809438f2a1842981a85
lordofgraves <achrafboutzate@gmail.com>
2023-05-09 10:10:21 -0700

    mm

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -3,40 +3,45 @@
  * read_textfile - reads a text file and prints it to the POSIX standard output.
  * @filename: a pointer to a const char string.
  * @letters: a size_t integer gives the number of letters to read.
- * @fp: used to access the file.
  * @textbuffer: used to temporarily store data read from the file.
  * return: 0
  */
 
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-    FILE* fp;
-    char textbuffer[2048];
-    int fopen;
-    ssize_t fread, fwrite, fclose;
-    
-    fp = fopen(filename, "r");
-    if (fp == NULL)
-    {
-        printf("Error: can't open file\n");
-        return (0);
-    }
-    fread = 0;
-    while ((fread = fread(textbuffer, 1, sizeof(textbuffer), fp)) > 0)
-    {
-        fwrite = fwrite(textbuffer, 1, fread, stdout);
-        if (fwrite < 0)
-        {
-            printf("Error: can't write to stdout\n");
-            return (0);
-        }
-    }
-    
-    fclose = fclose(fp);
-    if (fclose != 0)
-    {
-        printf("Error: can't close the file\n");
-        return (0);
-    }
-    return (fread);
+	char *textbuffer;
+	int fopen;
+	ssize_t fread, fwrite, fclose;
+
+	if (filename == NULL)
+		return (0);
+
+	textbuffer = malloc(sizeof(char) * letters);
+	if (textbuffer == NULL)
+		return (0);
+
+	fopen = open(filename, O_RDONLY);
+
+	if (fopen == NULL)
+	{
+		free(textbuffer);
+		return (0);
+	}
+
+	fread = read(fopen, textbuffer, letters);
+
+	if (fread == NULL)
+		return (0);
+
+	fwrite = write(STDOUT_FILENO, textbuffer, fread);
+
+	if (fwrite == NULL)
+		return (0);
+
+	fclose = close(fopen);
+
+	if (fclose == NULL)
+		return (0);
+
+	return (fread);
 }
