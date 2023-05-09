d5c855af5d27cf4b94050c4285225304a46c947b
lordofgraves <achrafboutzate@gmail.com>
2023-05-09 10:33:02 -0700

    mm

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -4,25 +4,20 @@
  * @filename: a pointer to a const char string.
  * @letters: a size_t integer gives the number of letters to read.
  * @textbuffer: used to temporarily store data read from the file.
- * return: 0
+ * return: number of letters or 0
  */
 
 ssize_t read_textfile(const char *filename, size_t letters)
 {
+	ssize_t fopen, fread, fwrite;
 	char *textbuffer;
-	int fopen;
-	ssize_t fread, fwrite, fclose;
+
+	textbuffer = malloc(letters);
+	if (textbuffer == NULL)
+		return (0);
 
 	if (filename == NULL)
-	{
 		return (0);
-	}
-
-	textbuffer = malloc(sizeof(char) * letters);
-	if (textbuffer == NULL)
-	{
-		return (0);
-	}
 
 	fopen = open(filename, O_RDONLY);
 
@@ -34,24 +29,9 @@ ssize_t read_textfile(const char *filename, size_t letters)
 
 	fread = read(fopen, textbuffer, letters);
 
-	if (fread == -1)
-	{
-		return (-1);
-	}
-
 	fwrite = write(STDOUT_FILENO, textbuffer, fread);
 
-	if (fwrite == -1)
-	{
-		return (-1);
-	}
+	close(fopen);
 
-	fclose = close(fopen);
-
-	if (fclose == -1)
-	{
-		return (-1);
-	}
-
-	return (fread);
+	return (fwrite);
 }
