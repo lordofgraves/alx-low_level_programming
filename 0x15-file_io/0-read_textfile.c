71befd646bec2bfdf6b1bab98606855838431ee8
lordofgraves <achrafboutzate@gmail.com>
2023-05-09 10:05:41 -0700

    mm

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -5,8 +5,6 @@
  * @letters: a size_t integer gives the number of letters to read.
  * @fp: used to access the file.
  * @textbuffer: used to temporarily store data read from the file.
- * @total_read: total number of bytes read.
- * @n_read: store the number of bytes read by fread.
  * return: 0
  */
 
@@ -14,33 +12,31 @@ ssize_t read_textfile(const char *filename, size_t letters)
 {
     FILE* fp;
     char textbuffer[2048];
-    ssize_t total_read = 0, n_read;
-
-    if (filename == NULL)
-    {
-        printf("Error: the filename is NULL\n");
-        return (0);
-    }
-
+    int fopen;
+    ssize_t fread, fwrite, fclose;
+    
     fp = fopen(filename, "r");
     if (fp == NULL)
     {
-        printf("Error: can't open file %s\n", filename);
+        printf("Error: can't open file\n");
         return (0);
     }
-
-    while ((n_read = fread(textbuffer, 1, sizeof(textbuffer), fp)) > 0)
+    fread = 0;
+    while ((fread = fread(textbuffer, 1, sizeof(textbuffer), fp)) > 0)
     {
-        total_read += n_read;
-        if (fwrite(textbuffer, 1, n_read, stdout) != n_read)
+        fwrite = fwrite(textbuffer, 1, fread, stdout);
+        if (fwrite < 0)
         {
-            printf("Error: write is failed\n");
-            fclose(fp);
+            printf("Error: can't write to stdout\n");
             return (0);
         }
-        if (total_read >= letters) break;
     }
-
-    fclose(fp);
-    return total_read;
+    
+    fclose = fclose(fp);
+    if (fclose != 0)
+    {
+        printf("Error: can't close the file\n");
+        return (0);
+    }
+    return (fread);
 }
